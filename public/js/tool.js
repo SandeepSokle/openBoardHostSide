cf93236e655c735b5fcbba6d5c14e16d123d1f2a
SandeepSokle <60534028+SandeepSokle@users.noreply.github.com>
2021-07-22 13:38:41 +0530

    Update tool.js

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("https://opencontrols.herokuapp.com/");
+const socket = io.connect("https://opencontrolfinal.herokuapp.com/");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
